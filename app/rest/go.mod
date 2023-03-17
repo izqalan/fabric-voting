939d930313565826d45d07e0a73a91ae7ac41d4f
Izqalan Nor'izad <izqalan@gmail.com>
2023-03-17 15:46:35 +0800

    aded election create and fetch by id

--- app/rest/go.mod
+++ app/rest/go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0-rc.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hyperledger/fabric-gateway v1.2.2 // indirect
 	github.com/hyperledger/fabric-protos-go-apiv2 v0.3.0 // indirect
--- app/rest/routes/routes.go
+++ app/rest/routes/routes.go
@@ -14,12 +14,28 @@ type candidate struct {
 	ElectionID string `json:"electionID"`
 }
 
+type election struct {
+	ElectionID   string `json:"electionID"`
+	ElectionName string `json:"electionName"`
+	StartDate    string `json:"startDate"`
+	EndDate      string `json:"endDate"`
+}
+
 func SetupRouter(contract *client.Contract) *gin.Engine {
 	r := gin.Default()
 	v1 := r.Group("/api/v1")
 	{
 		v1.GET("/ping", pong)
 		v1.GET("/hello", helloWorld)
+		v1.POST("/candidate", func(c *gin.Context) {
+			createCandidate(contract, c)
+		})
+		v1.POST("/election", func(c *gin.Context) {
+			createElection(contract, c)
+		})
+		v1.GET("/election/:electionID", func(c *gin.Context) {
+			getElectionById(contract, c)
+		})
 	}
 	return r
 }
@@ -75,5 +91,59 @@ func createCandidate(contract *client.Contract, c *gin.Context) {
 	// retunr in json
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Candidate created. Txn committed successfully.",
+	})
+}
+
+// @Summary Create Election
+// @Description Create a new election
+// @Tags Election
+// @Accept  json
+// @Produce  json
+// @Body  {object} election
+// @Success 200 {string} string "Election created"
+// @Router /election [post]
+func createElection(contract *client.Contract, c *gin.Context) {
+
+	var election election
+	if err := c.ShouldBindJSON(&election); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err := contract.SubmitTransaction("createElection", election.ElectionName, election.StartDate, election.EndDate, election.ElectionID)
+	if err != nil {
+		panic(fmt.Errorf("failed to submit transaction: %w", err))
+	}
+
+	fmt.Printf("*** Transaction committed successfully\n")
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Election created. Txn committed successfully.",
+	})
+}
+
+// @Summary Get Election
+// @Description Get election by electionID
+// @Tags Election
+// @Accept  json
+// @Produce  json
+// @Param electionID path string true "Election ID"
+// @Success 200 {string} string "Election created"
+// @Router /election/{electionID} [get]
+func getElectionById(contract *client.Contract, c *gin.Context) {
+	electionID := c.Param("electionID")
+	result, err := contract.EvaluateTransaction("getElectionById", electionID)
+	if err != nil {
+		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
+	}
+
+	fmt.Printf("*** Transaction result: %s\n", string(result))
+
+	r := string(result)
+	// out, _ := json.Marshal(string(r))
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Election fetched successfully.",
+		"data":    r,
 	})
 }
