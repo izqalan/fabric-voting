439397fb5f6d308d9cdcb6f9dd7c29d0640d87fe
Izqalan Nor'izad <izqalan@gmail.com>
2023-03-27 15:23:10 +0800

    added create new election modal

--- app/fabric-voting-client/pages/admin/election.tsx
+++ app/fabric-voting-client/pages/admin/election.tsx
@@ -1,77 +1,103 @@
-import React from "react"
-import ElectionCard from "@/components/elements/electionCard"
-import ElectionDetails from "@/components/layouts/ElectionDetails"
-import { addDays, subDays } from "date-fns"
+import React from "react";
+import ElectionCard from "@/components/elements/electionCard";
+import ElectionDetails from "@/components/layouts/ElectionDetails";
+import { addDays, subDays, format, isValid } from "date-fns";
 import {
   useDisclosure,
   Flex,
-} from "@chakra-ui/react"
-
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
 
 export default function Election() {
-
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = React.useRef(null)
+  const {
+    isOpen: isNewElectionModalOpen,
+    onOpen: onNewElectionModalOpen,
+    onClose: onNewElectionModalClose,
+  } = useDisclosure();
+  const {
+    isOpen: isDatePickerOpen,
+    onOpen: onDatePickerOpen,
+    onClose: onDatePickerClose,
+  } = useDisclosure();
+  const btnRef = React.useRef(null);
 
   return (
-    <Flex
-      direction="row"
-      flexWrap="wrap"
-    >
-      <Flex direction="column" w="40%" px={2} overflow="scroll" maxHeight={"85vh"}>
-        <ElectionCard
-          ref={btnRef}
-          electionName="Pilihan Raya Univ. Malaya #99"
-          electionID="election.88888888"
-          startDate={subDays(new Date(), 2)}
-          endDate={addDays(new Date(), 7)}
-          createdAt={subDays(new Date(), 2)}
-          updatedAt={subDays(new Date(), 1)}
-          style={{ marginBottom: 16 }}
-          onClick={onOpen}
-        />
-        <ElectionCard
-          ref={btnRef}
-          electionName="Pilihan raya kampus UKM #42"
-          electionID="election.1234567890"
-          startDate={subDays(new Date(), 7)}
-          endDate={subDays(new Date(), 2)}
-          createdAt={subDays(new Date(), 7)}
-          updatedAt={subDays(new Date(), 7)}
-          style={{ marginBottom: 16 }}
-        />
-        <ElectionCard
-          ref={btnRef}
-          electionName="Pilihan raya kampus UKM #42"
-          electionID="election.1234567890"
-          startDate={subDays(new Date(), 7)}
-          endDate={subDays(new Date(), 2)}
-          createdAt={subDays(new Date(), 7)}
-          updatedAt={subDays(new Date(), 7)}
-          style={{ marginBottom: 16 }}
-        />
-        <ElectionCard
-          ref={btnRef}
-          electionName="Pilihan raya kampus UKM #42"
-          electionID="election.1234567890"
-          startDate={subDays(new Date(), 7)}
-          endDate={subDays(new Date(), 2)}
-          createdAt={subDays(new Date(), 7)}
-          updatedAt={subDays(new Date(), 7)}
-          style={{ marginBottom: 16 }}
-        />
-        <ElectionCard
-          ref={btnRef}
-          electionName="Pilihan raya kampus UKM #42"
-          electionID="election.1234567890"
-          startDate={subDays(new Date(), 7)}
-          endDate={subDays(new Date(), 2)}
-          createdAt={subDays(new Date(), 7)}
-          updatedAt={subDays(new Date(), 7)}
-          style={{ marginBottom: 16 }}
-        />
+    <Flex direction="row" flexWrap="wrap">
+      <Flex direction="column" w="40%" px={2}>
+        <Button
+          variant={"outline"}
+          color={"purple.500"}
+          borderColor={"purple.500"}
+          _hover={{ bg: "purple.500", color: "white" }}
+          mb={4}
+          onClick={onNewElectionModalOpen}
+        >
+          New election
+        </Button>
+        <Flex direction="column" overflow="scroll" maxHeight={"85vh"}>
+          <ElectionCard
+            ref={btnRef}
+            electionName="Pilihan Raya Univ. Malaya #99"
+            electionID="election.88888888"
+            startDate={subDays(new Date(), 2)}
+            endDate={addDays(new Date(), 7)}
+            createdAt={subDays(new Date(), 2)}
+            updatedAt={subDays(new Date(), 1)}
+            style={{ marginBottom: 16 }}
+          />
+          <ElectionCard
+            ref={btnRef}
+            electionName="Pilihan raya kampus UKM #42"
+            electionID="election.1234567890"
+            startDate={subDays(new Date(), 7)}
+            endDate={subDays(new Date(), 2)}
+            createdAt={subDays(new Date(), 7)}
+            updatedAt={subDays(new Date(), 7)}
+            style={{ marginBottom: 16 }}
+          />
+          <ElectionCard
+            ref={btnRef}
+            electionName="Pilihan raya kampus UKM #42"
+            electionID="election.1234567890"
+            startDate={subDays(new Date(), 7)}
+            endDate={subDays(new Date(), 2)}
+            createdAt={subDays(new Date(), 7)}
+            updatedAt={subDays(new Date(), 7)}
+            style={{ marginBottom: 16 }}
+          />
+          <ElectionCard
+            ref={btnRef}
+            electionName="Pilihan raya kampus UKM #42"
+            electionID="election.1234567890"
+            startDate={subDays(new Date(), 7)}
+            endDate={subDays(new Date(), 2)}
+            createdAt={subDays(new Date(), 7)}
+            updatedAt={subDays(new Date(), 7)}
+            style={{ marginBottom: 16 }}
+          />
+          <ElectionCard
+            ref={btnRef}
+            electionName="Pilihan raya kampus UKM #42"
+            electionID="election.1234567890"
+            startDate={subDays(new Date(), 7)}
+            endDate={subDays(new Date(), 2)}
+            createdAt={subDays(new Date(), 7)}
+            updatedAt={subDays(new Date(), 7)}
+            style={{ marginBottom: 16 }}
+          />
+        </Flex>
       </Flex>
-      <ElectionDetails 
+      <ElectionDetails
         electionName="Pilihan Raya Univ. Malaya #99"
         electionID="election.88888888"
         startDate={subDays(new Date(), 2)}
@@ -79,6 +105,32 @@ export default function Election() {
         createdAt={subDays(new Date(), 2)}
         updatedAt={subDays(new Date(), 1)}
       />
+
+      <Modal initialFocusRef={btnRef} isOpen={isNewElectionModalOpen} onClose={onNewElectionModalClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create election</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Election Name</FormLabel>
+              <Input ref={btnRef} placeholder="Eg: PRU Ke-14" />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Start date</FormLabel>
+              
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Save
+            </Button>
+            <Button onClick={onNewElectionModalClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
-  )
+  );
 }
