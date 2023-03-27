8a62c295442beaed4b6a781a46c307ab6b6f1bc3
Izqalan Nor'izad <izqalan@gmail.com>
2023-03-27 12:25:03 +0800

    result page overflow scroll

--- app/fabric-voting-client/pages/admin/result.tsx
+++ app/fabric-voting-client/pages/admin/result.tsx
@@ -9,7 +9,7 @@ export default function Result() {
 
   return (
     <Flex direction="row" flexWrap="wrap">
-      <Flex direction="column" w="40%" px={2}>
+      <Flex direction="column" w="40%" px={2} overflow="scroll" maxHeight={"85vh"}>
         <ElectionCard
           ref={btnRef}
           electionName="Pilihan raya kampus UKM #40"
@@ -18,6 +18,26 @@ export default function Result() {
           endDate={subDays(new Date(), 18)}
           createdAt={subDays(new Date(), 30)}
           updatedAt={subDays(new Date(), 29)}
+          style={{ marginBottom: 16 }}
+        />
+        <ElectionCard
+          ref={btnRef}
+          electionName="Pilihan Raya Univ. Malaya #98"
+          electionID="election.88888888"
+          startDate={subDays(new Date(), 365)}
+          endDate={subDays(new Date(), 355)}
+          createdAt={subDays(new Date(), 365)}
+          updatedAt={subDays(new Date(), 365)}
+          style={{ marginBottom: 16 }}
+        />
+        <ElectionCard
+          ref={btnRef}
+          electionName="Pilihan Raya Univ. Malaya #98"
+          electionID="election.88888888"
+          startDate={subDays(new Date(), 365)}
+          endDate={subDays(new Date(), 355)}
+          createdAt={subDays(new Date(), 365)}
+          updatedAt={subDays(new Date(), 365)}
           style={{ marginBottom: 16 }}
         />
         <ElectionCard
