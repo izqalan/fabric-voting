cc2551f057b3c1db91634936f6212287db70fa3d
Izqalan N <izqalan@gmail.com>
2023-06-21 02:38:08 +0800

    added load testing script

---
+++ app/rest/test/test.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type ElectionRequest struct {
+	ElectionName string `json:"electionName"`
+	StartDate    string `json:"startDate"`
+	EndDate      string `json:"endDate"`
+}
+
+type CandidateRequest struct {
+	Name       string `json:"name"`
+	StudentID  string `json:"studentID"`
+	ElectionID string `json:"electionID"`
+	Faculty    string `json:"faculty"`
+	Party      string `json:"party"`
+	Avatar     string `json:"avatar"`
+}
+
+type VoteRequest struct {
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	ElectionID  string `json:"electionID"`
+	CandidateID string `json:"candidateID"`
+}
+
+func main() {
+	// Configuration
+	concurrentRequests := 20                            // Total number of requests to be sent
+	apiURL := "http://localhost/api/v2/ballot/vote"     // URL of the voting API endpoint
+	electionURL := "http://localhost/api/v1/election"   // URL of the election API endpoint
+	candidateURL := "http://localhost/api/v1/candidate" // URL of the candidate API endpoint
+	AVATAR_URL := "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1061&q=80"
+
+	// Create an election
+	// format start date and end date to RFC3339
+	startDate := time.Now().UTC().Format(time.RFC3339)
+	date := time.Now()
+	endDate := date.AddDate(0, 0, 1).UTC().Format(time.RFC3339)
+
+	electionReq := ElectionRequest{
+		ElectionName: "Load testing election",
+		StartDate:    startDate,
+		EndDate:      endDate,
+	}
+
+	requestBody, err := json.Marshal(electionReq)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Create a new POST request for election creation
+	resp, err := http.Post(electionURL, "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Get the electionID from the response body.data.electionID
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	var electionResponse map[string]interface{}
+	err = json.Unmarshal(body, &electionResponse)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	electionID := electionResponse["data"].(map[string]interface{})["electionID"].(string)
+
+	fmt.Println("Election created successfully")
+
+	// Create candidates
+	for i := 0; i < 2; i++ {
+		candidateReq := CandidateRequest{
+			Name:       "Candidate " + strconv.Itoa(i+1), // Customize with appropriate values
+			StudentID:  "A00000" + strconv.Itoa(i+1),     // Customize with appropriate values
+			ElectionID: electionID,                       // Customize with appropriate values
+			Faculty:    "ftsm",                           // Customize with appropriate values
+			Party:      "Party " + strconv.Itoa(i+1),     // Customize with appropriate values
+			Avatar:     AVATAR_URL,                       // Customize with appropriate values
+		}
+
+		requestBody, err := json.Marshal(candidateReq)
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		// Create a new POST request for candidate creation
+		resp, err := http.Post(candidateURL, "application/json", bytes.NewBuffer(requestBody))
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+		defer resp.Body.Close()
+
+		_, err = io.Copy(ioutil.Discard, resp.Body)
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		fmt.Printf("Candidate %d created successfully\n", i+1)
+	}
+
+	// Start the load testing
+	// fmt.Printf("Sending %d voting requests...\n", totalRequests)
+	startTime := time.Now()
+
+	// Create a wait group to synchronize goroutines
+	var wg sync.WaitGroup
+	wg.Add(concurrentRequests)
+	responseTimes := make(chan time.Duration, concurrentRequests)
+
+	for i := 0; i < concurrentRequests; i++ {
+		go func(i int) {
+			defer wg.Done()
+			fmt.Printf("Sending request %d...\n", i+1)
+			// Prepare the request body
+			email := "a" + strconv.Itoa(i+1) + "@siswa.ukm.edu.my" // Generate email based on iteration
+			ledgerElectionID := electionID
+			voteReq := VoteRequest{
+				Email:       email,
+				Password:    "P5ssw0rd",        // Customize with appropriate values
+				ElectionID:  ledgerElectionID,  // Customize with appropriate values
+				CandidateID: getCandidateID(i), // Get candidate ID based on the value of `i`
+			}
+
+			requestBody, err := json.Marshal(voteReq)
+
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+
+			// Create a new POST request for voting
+			start := time.Now()
+			resp, err := http.Post(apiURL, "application/json", bytes.NewBuffer(requestBody))
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+			defer resp.Body.Close()
+			duration := time.Since(start)
+
+			_, err = io.Copy(ioutil.Discard, resp.Body)
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+
+			// print status code and message
+
+			//get response time from api
+
+			responseTimes <- duration
+
+			fmt.Printf("Request: %s | Status Code: %d | Response time: %s\n", email, resp.StatusCode, duration)
+		}(i)
+
+	}
+	wg.Wait()
+
+	close(responseTimes)
+
+	// Calculate total duration
+	totalDuration := time.Since(startTime)
+	averageResponseTime := calculateAverage(responseTimes)
+
+	// Print results
+	fmt.Printf("Load test completed in %s\n", totalDuration)
+	fmt.Printf("Average response time: %s\n", averageResponseTime)
+
+}
+
+// Helper function to get the candidate ID based on the value of `i`
+func getCandidateID(i int) string {
+	if i%2 == 0 {
+		return "candidate.A000001" // Customize with appropriate candidate ID
+	} else {
+		return "candidate.A000002" // Customize with appropriate candidate ID
+	}
+}
+
+func calculateAverage(times <-chan time.Duration) time.Duration {
+	var sum time.Duration
+	count := 0
+
+	for t := range times {
+		sum += t
+		count++
+	}
+
+	if count > 0 {
+		return time.Duration(int64(sum) / int64(count))
+	}
+
+	return 0
+}
